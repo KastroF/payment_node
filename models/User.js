3427d48ab16136d48073fb3d8a08972ee1f84e25
KastroF <nkastrro@gmail.com>
2025-07-17 15:15:08 +0100

    Last test 8

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const userSchema = mongoose.Schema({
         name: {type: String},
         username: {type: String}, 
         password: {type: String}, 
-        app: {type: String}
+        app: {type: String}, 
+        userActive: {type: Boolean}
 })
 
 module.exports = mongoose.model("User", userSchema);
